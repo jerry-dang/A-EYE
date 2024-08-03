74d41d912c25c462d2b7dcf3065575f7ea03d62f
k26le <k26le@uwaterloo.ca>
2024-08-03 03:08:04 -0400

    add more test images

--- facial-recognition/facial_expression.py
+++ facial-recognition/facial_expression.py
@@ -7,9 +7,9 @@ class FacialExpressionRecognition:
     def __init__(self):
         print("hi")
 
-    def process_image(self):
+    def process_image(self, image=None):
         # grab locally # TODO replace
-        analysis = DeepFace.analyze(img_path="./test_images/leo_neutral.jpg", actions=["emotion"])
+        analysis = DeepFace.analyze(img_path="./test_images/girl_long_hair_4.png", actions=["emotion"])
         dominant_emotion = analysis[0]["dominant_emotion"]
         print(dominant_emotion)
         return dominant_emotion
