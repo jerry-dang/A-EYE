5b09c60c7622321a82693a6e925ce7174ad1b506
Jerry Dang <59210998+jerry-dang@users.noreply.github.com>
2024-08-07 23:48:08 -0400

    Update settings.py

--- backend/aeye/aeye/settings.py
+++ backend/aeye/aeye/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+### TODO: Replace SECRET_KEY with actual key ###
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
